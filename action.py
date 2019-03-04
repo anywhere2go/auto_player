31ef6d30a7ed38d02198d991d66a59103f947cc7
liqi <63397520@qq.com>
2019-03-04 18:01:32 +0800

    update

--- action.py
+++ action.py
@@ -1,34 +1,47 @@
-import cv2,numpy,time, os, random, subprocess
+
+import cv2,numpy,time,os, random
+from PIL import ImageGrab
 from winsound import Beep
 
-#ADB命令手机截屏，并发送到当前目录,opencv读取文件并返回
-def screen_shot(): 
-    a = "adb shell screencap -p /data/data/screen.jpg"
-    b = "adb pull /data/data/screen.jpg"
+ #在背景查找目标图片，并返回查找到的结果坐标列表，target是背景，want是要找目标
+def locate(target,want, show=0, msg=0):
+    loc_pos=[]
+    want,treshold,c_name=want[0],want[1],want[2]
+    result=cv2.matchTemplate(target,want,cv2.TM_CCOEFF_NORMED)
+    location=numpy.where(result>=treshold)
 
-    command_list = [a, b]
-    for i in command_list:
-        os.system(i)
+    if msg:  #显示正式寻找目标名称，调试时开启
+        print(c_name,'searching... ')
 
-    screen=cv2.imread('screen.jpg')
-    return screen
+    h,w=want.shape[:-1] #want.shape[:-1]
 
-# ADB命令点击屏幕，参数pos为目标坐标
-def touch(pos):
-    x, y = pos
-    a = "adb shell input touchscreen tap {0} {1}" .format(x, y)
-    os.system(a)
+    n,ex,ey=1,0,0
+    for pt in zip(*location[::-1]):    #其实这里经常是空的
+        x,y=pt[0]+int(w/2),pt[1]+int(h/2)
+        if (x-ex)+(y-ey)<15:  #去掉邻近重复的点
+            continue
+        ex,ey=x,y
 
-#蜂鸣报警器，参数n为鸣叫资料
-def alarm(n):
-    frequency = 1500
-    last = 500
+        cv2.circle(target,(x,y),10,(0,0,255),3)
 
-    for n in range(n):   
-        Beep(frequency,last)
-        time.sleep(0.05)
+        if msg:
+            print(c_name,'we find it !!! ,at',x,y)
+            
+        x,y=int(x),int(y)
+        loc_pos.append([x,y])
 
-#按【文件内容，匹配精度，名称】格式批量读取要查找的目标图片，精度统一为0.85，名称为文件名
+    if show:  #在图上显示寻找的结果，调试时开启
+        cv2.imshow('we get',target)
+        cv2.waitKey(0) 
+        cv2.destroyAllWindows()
+
+    if len(loc_pos)==0:
+        print(c_name,'not find')
+
+    return loc_pos
+
+
+#按【文件内容，匹配精度，名称】格式批量聚聚要查找的目标图片，精度统一为0.85，名称为文件名
 def load_imgs():
     mubiao = {}
     path = os.getcwd() + '/jpg'
@@ -42,44 +55,21 @@ def load_imgs():
 
     return mubiao
 
- #在背景查找目标图片，以列表形式返回查找目标的中心坐标，
- #screen是截屏图片，want是找的图片【按上面load_imgs的格式】，show是否以图片形式显示匹配结果【调试用】
-def locate(screen, want, show=0):
-    loc_pos = []
-    want, treshold, c_name = want[0], want[1], want[2]
-    result = cv2.matchTemplate(screen, want, cv2.TM_CCOEFF_NORMED)
-    location = numpy.where(result >= treshold)
+#蜂鸣报警器，参数n为鸣叫资料
+def alarm(n):
+    frequency = 1500
+    last = 500
 
-    h,w = want.shape[:-1] 
-
-    n,ex,ey = 1,0,0
-    for pt in zip(*location[::-1]):   
-        x,y = pt[0] + int(w/2), pt[1] + int(h/2)
-        if (x-ex) + (y-ey) < 15:  #去掉邻近重复的点
-            continue
-        ex,ey = x,y
-
-        cv2.circle(screen, (x, y), 10, (0, 0, 255), 3)
-            
-        x,y = int(x), int(y)
-        loc_pos.append([x, y])
-
-    if show:  #在图上显示寻找的结果，调试时开启
-        cv2.imshow('we get', screen)
-        cv2.waitKey(0) 
-        cv2.destroyAllWindows()
-
-    if len(loc_pos) == 0:
-        print(c_name, 'not found')
-
-    return loc_pos
+    for n in range(n):   
+        Beep(frequency,last)
+        time.sleep(0.05)
 
 #裁剪图片以缩小匹配范围，screen为原图内容，upleft、downright是目标区域的左上角、右下角坐标
-def cut(screen, upleft, downright): 
+def cut(screen,upleft,downright): 
 
-    a,b = upleft
-    c,d = downright
-    screen = screen[b:d,a:c]
+    a,b=upleft
+    c,d=downright
+    screen=screen[b:d,a:c]
 
     return screen
 
@@ -91,10 +81,3 @@ def cheat(p, w, h):
     e,f = a + c, b + d
     y = [e, f]
     return(y)
-
-#随机延迟，防止游戏外挂检测，延迟时间范围为【x, y】秒之间
-def wait(x, y):
-    t = random.uniform(x, y)
-    time.sleep(t)
-
-
--- yys.py
+++ yys.py
@@ -1,164 +1,360 @@
-import cv2, time, random, os
+import cv2,numpy,time,random
+import os,sys,pyautogui, traceback
+from PIL import ImageGrab
 import action
 
-debug_mode = 0
-
-# 读取文件
+# 读取文件 精度控制   显示名字
 imgs = action.load_imgs()
+pyautogui.PAUSE = 0.1
 
-os.system("adb devices")
 start_time = time.time()
 print('程序启动，现在时间', time.ctime())
-print('菜单：1 结界自动合卡，2 自动通关魂十，3 自动通关业原火，4 自动刷组队狗粮（打手模式） ')
+
+
 #以上启动，载入设置
-
 ##########################################################
-if debug_mode:
-    mode = debug_mode
-else:
-    raw = input("选择功能模式：")
-    mode = int(raw)
 
-action.alarm(1)
-print("开始运行")
+def log(f):
+    def wrap(*agrs, **kwagrs):
+        try:
+            ans = f(*agrs, **kwagrs)
+            return ans
+        except:
+            traceback.print_exc()
+            time.sleep(60)
+
+    return wrap
+
+@log
+def select_mode():
+    print('''\n菜单：  鼠标移动到最右侧中止并返回菜单页面, 
+        1 结界自动合卡，自动选择前三张合成 
+        2 自动通关魂十，自动接受组队并确认通关
+        3 自动通关业原火，单刷
+        4 自动刷组队狗粮（打手模式），          
+        5 单刷探索副本，无法区分经验BUFF
+        ''')
+    action.alarm(1)
+    raw = input("选择功能模式：")
+    index = int(raw)
+
+    mode = [0, card, yuhun, yeyuanhuo, goliang, solo]
+    comand = mode[index]
+    comand()
 
 ##########################################################
 #合成结界卡，较简单，未偏移直接点
-while mode == 1:
-    x, y, z = (370, 238), (384, 385), (391, 525)  #前三张卡的位置
-    zz = (871, 615)               #合成按钮位置
-    for i in [x, y, z ,zz]:
-        action.touch(i)
-        time.sleep(0.1)
-    time.sleep(0.2)
+def card():
+    while True:
+        #鼠标移到右侧中止    
+        if pyautogui.position()[0] >= pyautogui.size()[0] * 0.7:
+            select_mode()
+            
+        x, y, z = (370, 238), (384, 385), (391, 525)  #前三张卡的位置
+        zz = (871, 615)               #合成按钮位置
+        for i in [x, y, z ,zz]:
+            pyautogui.click(i)
+            time.sleep(0.1)
+        time.sleep(0.5)
+
 
 ########################################################
 #魂十通关
-while mode == 2 :   
-    screen = action.screen_shot()
-    
-    print('screen shot ok',time.ctime())
-    
-    #设定目标，开始查找
-    #自动点击通关结束后的页面
-    for i in ['ying','jiangli','kaishi','jixu' ,'zhunbei']:
-        want = imgs[i]
-        size = want[0].shape
-        h, w , ___ = size
-        target = screen
-        pts = action.locate(target, want, 0)
-        if not len(pts) == 0:
-            for pt in pts:
-                pt = action.cheat(pt, w, h)
-                action.touch(pt)
-            break
+def yuhun():
+    while True :
+        #鼠标移到最右侧中止    
+        if pyautogui.position()[0] >= pyautogui.size()[0] * 0.98:
+            select_mode()
+
+        screen=ImageGrab.grab()
+        screen.save('screen.jpg')
+        screen=cv2.imread('screen.jpg')
+
+        #截屏，并裁剪以加速
+        upleft=(0, 0)
+        downright=(2550, 770) #上部并排
+
+        a,b=upleft
+        c,d=downright
+        screen=screen[b:d,a:c]
+
+        print('screen shot ok',time.ctime())
+        
+        #设定目标，开始查找
+        #这里是自动接受组队
+        for i in ['jieshou2',"jieshou"]:
+            want = imgs[i]
+            size = want[0].shape
+            h, w , ___ = size
+            x1,x2 = upleft, (430, 358)
+            target = action.cut(screen, x1, x2)
+            pts = action.locate(target,want,0)
+            if not len(pts) == 0:
+                print('接受组队')
+                xx = pts[0]
+                xx = action.cheat(xx, w, h)
+                if xx[0] > 120:           
+                    pyautogui.click(xx)
+                    t = random.randint(40,80) / 100
+                    time.sleep(t)
+                    break
+                else:
+                    pass
+                continue
+
+        #自动点击通关结束后的页面
+        for i in ['ying','jiangli','kaishi','jixu' ]:
+            want = imgs[i]
+            size = want[0].shape
+            h, w , ___ = size
+            target = screen
+            pts = action.locate(target,want,0)
+            if not len(pts) == 0:
+                for pt in pts:
+                    pt = action.cheat(pt, w, h)
+                    pyautogui.click(pt)
+                    t = random.randint(100,200) / 1000
+                    time.sleep(t)
+                break
+    select_mode()
 
 ########################################################
 #业原火通关
-while mode == 3 :   #直到取消，或者出错
-    screen = action.screen_shot()
+def yeyuanhuo():
+    while True :   #直到取消，或者出错
+        if pyautogui.position()[0] >= pyautogui.size()[0] * 0.98:
+            select_mode()
 
-    print('screen shot ok',time.ctime())
-    
-    #设定目标，开始查找
+        screen=ImageGrab.grab()
+        screen.save('screen.jpg')
+        screen=cv2.imread('screen.jpg')
 
-    #过关
-    for i in ['ying','jiangli','tiaozhan','jixu']:
-        want = imgs[i]
-        size = want[0].shape
-        h, w , ___ = size
-        target = screen
-        pts=action.locate(target, want, 0)
-        if not len(pts)==0:
-            pt = pt[0]
-            pt = action.cheat(pt, w, h)
-            action.touch(pt)
-            break
+        #截屏，并裁剪以加速
+        upleft=(0, 0)
+        downright=(1426, 798)
 
+        a,b=upleft
+        c,d=downright
+        screen=screen[b:d,a:c]
+
+        print('screen shot ok',time.ctime())
+        
+        #设定目标，开始查找
+
+        #过关
+        for i in ['ying','jiangli','tiaozhan','jixu']:
+            want=imgs[i]
+            size = want[0].shape
+            h, w , ___ = size
+            target=screen
+            pts=action.locate(target,want,0)
+            if not len(pts)==0:
+                for pt in pts:
+                    pt = action.cheat(pt, w, h)
+                    pyautogui.click(pt)
+                    t = random.randint(20,50) / 100
+                    time.sleep(t)
+                break
 
 ########################################################
-#狗粮通关
-while mode == 4:   #直到取消，或者出错
-    screen = action.screen_shot()
+    #狗粮通关
+def goliang():
+    while True:   #直到取消，或者出错
+        if pyautogui.position()[0] >= pyautogui.size()[0] * 0.98:
+            select_mode()
 
-    #截屏，并裁剪以加速
-    upleft=(0, 0)
-    downright=(1358, 768)
-    downright2=(2550, 768)
+        screen=ImageGrab.grab()
+        screen.save('screen.jpg')
+        screen=cv2.imread('screen.jpg')
 
-    print('screen shot ok',time.ctime())
-    
-    #设定目标，开始查找
-    #进入地图后
-    want=imgs['guding'] #固定阵容，即在地图中
+        #截屏，并裁剪以加速
+        upleft=(0, 0)
+        downright=(1358, 768)
+        downright2=(2550, 768)
 
-    x1 = (785, 606)
-    x2 = downright
-    target = action.cut(screen, x1, x2)
-    pts = action.locate(target,want,0)
-    if not len(pts) == 0:
-        print('正在地图中')
+        a,b=upleft
+        c,d=downright
+        screen=screen[b:d,a:c]
+
+        print('screen shot ok',time.ctime())
         
-        want = imgs['xiao']  #笑脸表情，表示在组队状态中
-        x1,x2 = (5, 405), (119, 560)
+        #设定目标，开始查找
+        #进入后
+        want=imgs['guding']
+
+        x1 = (785, 606)
+        x2 = downright
         target = action.cut(screen, x1, x2)
-        pts = action.locate(target, want, 0)
+        pts = action.locate(target,want,0)
         if not len(pts) == 0:
-            print('组队状态中')
-        else:
-            print('退出重新组队')
+            print('正在地图中')
             
-            for i in ['queren', 'tuichu']: #否，即队友已退出，跟随退出重新组队
-                want = imgs[i]
-                size = want[0].shape
-                h, w , ___ = size
-                x1,x2 = upleft, (965, 522)
-                target = action.cut(screen, x1, x2)
-                pts = action.locate(target,want,0)
-                if not len(pts) == 0:
-                    print('退出中')
-                    try:
-                        queding = pts[1]
-                    except:
-                        queding = pts[0]
-                    queding = action.cheat(queding, w, h)
-                    action.touch(queding)
-                    break
+            want = imgs['xiao']
+            x1,x2 = (5, 405), (119, 560)
+            target = action.cut(screen, x1, x2)
+            pts = action.locate(target,want,0)
+            if not len(pts) == 0:
+                print('组队状态中')
+            else:
+                print('退出重新组队')
+                
+                for i in ['queren', 'tuichu']:
+                    want = imgs[i]
+                    size = want[0].shape
+                    h, w , ___ = size
+                    x1,x2 = upleft, (965, 522)
+                    target = action.cut(screen, x1, x2)
+                    pts = action.locate(target,want,0)
+                    if not len(pts) == 0:
+                        print('退出中')
+                        try:
+                            queding = pts[1]
+                        except:
+                            queding = pts[0]
+                        queding = action.cheat(queding, w, h)
+                        pyautogui.click(queding)
+                        t = random.randint(50,80) / 100
+                        time.sleep(t)
+                        break
+                continue
+
+        want = imgs['jieshou']
+        size = want[0].shape
+        h, w , ___ = size
+        x1,x2 = upleft, (250, 380)
+        target = action.cut(screen, x1, x2)
+        pts = action.locate(target,want,0)
+        if not len(pts) == 0:
+            print('接受组队')
+            xx = pts[0]
+            xx = action.cheat(xx, w, h)
+            if xx[0] > 120:           
+                pyautogui.click(xx)
+                t = random.randint(40,80) / 100
+                time.sleep(t)
+            else:
+                pass
             continue
 
-    #不在地图中，接受组队
-    want = imgs['jieshou']
-    size = want[0].shape
-    h, w , ___ = size
-    x1,x2 = upleft, (250, 380)
-    target = action.cut(screen, x1, x2)
-    pts = action.locate(target,want, 0)
-    if not len(pts) == 0:
-        print('接受组队')
-        xx = pts[0]
-        xx = action.cheat(xx, w, h)
-        if xx[0] > 120:           
-            action.touch(xx)
-        else:
-            pass
-        continue
+        for i in ['ying','jiangli','jixu']:
+            want = imgs[i]
+            size = want[0].shape
+            h, w , ___ = size
+            target = screen
+            pts = action.locate(target,want,0)
+            if not len(pts) == 0:
+                print('领取奖励')
+                xy = action.cheat(pts[0], w, h-10 )
+                pyautogui.click(xy)
+                t = random.randint(15,30) / 100
+                time.sleep(t)
+                break
 
-    #确认通关
-    for i in ['ying','jiangli','jixu']:
-        want = imgs[i]
+########################################################
+#单人探索
+def solo():
+    while True:   #直到取消，或者出错
+        if pyautogui.position()[0] >= pyautogui.size()[0] * 0.98:
+            select_mode()
+
+        screen=ImageGrab.grab()
+        screen.save('screen.jpg')
+        screen=cv2.imread('screen.jpg')
+
+        #截屏，并裁剪以加速
+        upleft=(0, 0)
+        downright=(1358, 768)
+        downright2=(2550, 768)
+
+        a,b=upleft
+        c,d=downright
+        screen=screen[b:d,a:c]
+
+        print('screen shot ok',time.ctime())
+        
+        #设定目标，开始查找
+        #进入后
+        want=imgs['guding']
+
+        x1 = (785, 606)
+        x2 = downright
+        target = action.cut(screen, x1, x2)
+        pts = action.locate(target,want,0)
+        if not len(pts) == 0:
+            print('正在地图中')
+            
+            want = imgs['left']
+            target = screen
+            pts = action.locate(target,want,0)
+            if not len(pts) == 0:
+                right = (854, 527)
+                right = action.cheat(right, 10, 10)
+                pyautogui.click(right)
+                t = random.randint(50,80) / 100
+                time.sleep(t)
+                continue
+
+            want = imgs['jian']
+            target = screen
+            pts = action.locate(target,want,0)
+            if not len(pts) == 0:
+                print('点击小怪')
+                xx = action.cheat(pts[0], 10, 10)        
+                pyautogui.click(xx)
+                time.sleep(0.5)
+                continue
+            else:
+                for i in ['queren', 'tuichu']:
+                    want = imgs[i]
+                    size = want[0].shape
+                    h, w , ___ = size
+                    x1,x2 = upleft, (965, 522)
+                    target = action.cut(screen, x1, x2)
+                    pts = action.locate(target,want,0)
+                    if not len(pts) == 0:
+                        print('退出中')
+                        try:
+                            queding = pts[1]
+                        except:
+                            queding = pts[0]
+                        queding = action.cheat(queding, w, h)
+                        pyautogui.click(queding)
+                        t = random.randint(50,80) / 100
+                        time.sleep(t)
+                        break
+                continue
+
+        for i in ['ying','jiangli','jixu']:
+            want = imgs[i]
+            size = want[0].shape
+            h, w , ___ = size
+            target = screen
+            pts = action.locate(target,want,0)
+            if not len(pts) == 0:
+                print('领取奖励')
+                xy = action.cheat(pts[0], w, h-10 )
+                pyautogui.click(xy)
+                t = random.randint(15,30) / 100
+                time.sleep(t)
+                break
+
+        want = imgs['tansuo']
         size = want[0].shape
         h, w , ___ = size
         target = screen
         pts = action.locate(target,want,0)
         if not len(pts) == 0:
-            print('领取奖励')
+            print('进入地图')
             xy = action.cheat(pts[0], w, h-10 )
-            action.touch(xy)
-            break
-    
-    #定时器，建议每次隔60分钟提醒一次，不要一次连续刷太久
-    now_time = time.time()
-    if now_time - start_time > 60 * 60:  #默认60分钟
-        start_time = now_time
-        action.alarm(10)
+            pyautogui.click(xy)
+            t = random.randint(15,30) / 100
+            time.sleep(t)
+
+####################################################
+
+
+
+
+if __name__ == '__main__':
+    select_mode()
 
