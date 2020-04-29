6742242efc14a7b4ccf505edaad2a48ef6d037c0
lisai9093 <lisai9093@gmail.com>
2020-04-29 16:24:02 -0700

    加入探索司机模式

--- yys.py
+++ yys.py
@@ -33,21 +33,23 @@ def select_mode():
         2 自动御魂通关(司机)
         3 自动御魂通关(打手)
         4 自动御魂通关(单刷)
-        5 自动探索副本(打手)，          
-        6 自动探索副本(单刷)
-        7 百鬼夜行
-        8 斗技
-        9 当前活动（万事屋）
-        10 结界自动合卡，自动选择前三张合成
-        11 抽卡
-        12 式神升星
+        5 自动探索副本(司机)
+        6 自动探索副本(打手)
+        7 自动探索副本(单刷)
+        8 百鬼夜行
+        9 斗技
+        10 当前活动（万事屋）
+        11 结界自动合卡，自动选择前三张合成
+        12 抽卡
+        13 式神升星
         ''')
     action.alarm(1)
     raw = input("选择功能模式：")
     index = int(raw)
 
     mode = [0, tupo, yuhun, yuhun2, yuhundanren,\
-            goliang, solo, baigui, douji, huodong,\
+            gouliang, gouliang2, gouliang3,\
+            baigui, douji, huodong,\
             card, chouka, shengxing]
     comand = mode[index]
     comand()
@@ -266,7 +268,7 @@ def yuhun2():
                 print('挑战中。。。',i)
                 xy = action.cheat(pts[0], w, h-10 )
                 pyautogui.click(xy)
-                t = random.randint(20,80) / 100
+                t = random.randint(80,150) / 100
                 time.sleep(t)
                 break
             
@@ -326,8 +328,135 @@ def yuhundanren():
                 break
 
 ########################################################
-#组队探索
-def goliang():
+#探索司机
+def gouliang():
+    while True:   #直到取消，或者出错
+        if pyautogui.position()[0] >= pyautogui.size()[0] * 0.98:
+            select_mode()
+
+        #截屏，并裁剪以加速
+        upleft = (0, 0)
+        downright = (1358, 900)
+        downright2 = (1280, 720)
+
+        a,b = upleft
+        c,d = downright
+
+        #截屏
+        monitor = {"top": b, "left": a, "width": c, "height": d}
+        im = np.array(mss.mss().grab(monitor))
+        screen = cv2.cvtColor(im, cv2.COLOR_BGRA2BGR)
+        #debug
+        #cv2.imshow('image',im)
+        #return
+        ############################
+        
+        #体力不足
+        want = imgs['notili']
+        size = want[0].shape
+        h, w , ___ = size
+        target = screen
+        pts = action.locate(target,want,0)
+        if not len(pts) == 0:
+            print('体力不足 ')
+            select_mode()
+
+        want = imgs['queren']
+        size = want[0].shape
+        h, w , ___ = size
+        target = screen
+        #x1,x2 = upleft, (965, 522)
+        #target = action.cut(screen, x1, x2)
+        pts = action.locate(target,want,0)
+        if not len(pts) == 0:
+            print('确认退出')
+            try:
+                queding = pts[1]
+            except:
+                queding = pts[0]
+            xy = action.cheat(queding, w, h)
+            pyautogui.click(xy)
+            pyautogui.moveTo(xy)
+            t = random.randint(15,30) / 100
+            time.sleep(t)
+
+        
+        #设定目标，开始查找
+        #进入后
+        want=imgs['guding']
+
+        x1 = (785, 606)
+        x2 = downright
+        target = action.cut(screen, x1, x2)
+        pts = action.locate(target,want,0)
+        if not len(pts) == 0:
+            print('正在地图中')
+            
+            want = imgs['left']
+            target = screen
+            pts = action.locate(target,want,0)
+            if not len(pts) == 0:
+                right = (854, 527)
+                right = action.cheat(right, 10, 10)
+                pyautogui.click(right)
+                t = random.randint(50,80) / 100
+                time.sleep(t)
+                continue
+
+            for i in ['boss', 'jian']:
+                want = imgs[i]
+                size = want[0].shape
+                h, w , ___ = size
+                target = screen
+                pts = action.locate(target,want,0)
+                if not len(pts) == 0:
+                    print('点击小怪',i)
+                    xx = action.cheat(pts[0], w, h)        
+                    pyautogui.click(xx)
+                    time.sleep(0.5)
+                    break
+
+            if i=='jian' and len(pts)==0:
+                for i in ['queren', 'tuichu']:
+                    want = imgs[i]
+                    size = want[0].shape
+                    h, w , ___ = size
+                    x1,x2 = upleft, (965, 522)
+                    target = action.cut(screen, x1, x2)
+                    pts = action.locate(target,want,0)
+                    if not len(pts) == 0:
+                        print('退出中',i)
+                        try:
+                            queding = pts[1]
+                        except:
+                            queding = pts[0]
+                        queding = action.cheat(queding, w, h)
+                        pyautogui.click(queding)
+                        t = random.randint(50,80) / 100
+                        time.sleep(t)
+                        break
+                continue
+
+        for i in ['jujue','queding','ying','jiangli','jixu','c28','zudui','ditu']:
+            want = imgs[i]
+            size = want[0].shape
+            h, w , ___ = size
+            target = screen
+            pts = action.locate(target,want,0)
+            if not len(pts) == 0:
+                print('领取奖励',i)
+                xy = action.cheat(pts[0], w, h )
+                pyautogui.click(xy)
+                if i=='queding':
+                    t = random.randint(150,200) / 100
+                else:
+                    t = random.randint(15,30) / 100
+                time.sleep(t)
+                break
+
+########################################################
+#探索打手
+def gouliang2():
     while True:   #直到取消，或者出错
         if pyautogui.position()[0] >= pyautogui.size()[0] * 0.98:
             select_mode()
@@ -418,10 +547,10 @@ def goliang():
                 t = random.randint(30,80) / 100
                 time.sleep(t)
                 break
-
+            
 ########################################################
-#单人探索
-def solo():
+#探索单人
+def gouliang3():
     while True:   #直到取消，或者出错
         if pyautogui.position()[0] >= pyautogui.size()[0] * 0.98:
             select_mode()
@@ -651,6 +780,7 @@ def baigui():
 #斗技
 def douji():
     doujiauto=True
+    doujipaidui=0
     while True:   #直到取消，或者出错
         if pyautogui.position()[0] >= pyautogui.size()[0] * 0.98:
             select_mode()
@@ -680,7 +810,8 @@ def douji():
         
         for i in ['jujue','queren','douji','douji2','doujiend','ying',\
                   'doujiqueren','doujiother','duanwei',\
-                  'liaotianguanbi','zhunbei','zhunbei2','tui']:
+                  'liaotianguanbi','zhunbei','zhunbei2','tui',\
+                  'doujiquxiao']:
             want = imgs[i]
             size = want[0].shape
             h, w , ___ = size
@@ -691,6 +822,7 @@ def douji():
                     #print('准备退出',i)
                     break
                 elif i=='douji':
+                    doujipaidui=0
                     doujiauto=True
                     print('斗技开始',i)
                     xy = action.cheat(pts[0], w, h-10 )
@@ -698,6 +830,17 @@ def douji():
                     t = random.randint(15,30) / 100
                     time.sleep(t)
                     break
+                elif i=='doujiquxiao':
+                    doujipaidui=doujipaidui+1
+                    print('斗技搜索:',doujipaidui)
+                    if doujipaidui>5:
+                        doujipaidui=0
+                        print('取消搜索')
+                        xy = action.cheat(pts[0], w, h-10 )
+                        pyautogui.click(xy)
+                        t = random.randint(15,30) / 100
+                        time.sleep(t)
+                        break
                 else:
                     print('斗技中。。。',i)
                     xy = action.cheat(pts[0], w, h-10 )
